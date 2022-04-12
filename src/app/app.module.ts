f091fbf41c9c78014b01becae7adc7ee5f31bc7a
Arick Antonio Roque Canelo <ARoque@minsa.gob.ni>
2022-04-12 10:14:55 -0600

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListadoComponent } from './heroes/listado/listado/listado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroeComponent,
+    ListadoComponent,
   ],
   imports: [
     BrowserModule,
